ee9c73d59a6e959bc3738e6c66bb12cc74997a78
alexui <alex.budau@yahoo.com>
2016-03-28 21:43:09 +0300

    finished Contacts Manager

---
+++ ContactsManager/app/src/main/java/com/politehnica/alexbudau/contactsmanager/Constants.java
@@ -0,0 +1,11 @@
+package com.politehnica.alexbudau.contactsmanager;
+
+/**
+ * Created by Alex on 3/28/2016.
+ */
+public class Constants {
+
+    public final static String PHONE_NUMBER_EXTRA = "PHONE_NUMBER";
+
+    public final static int CONTACTS_CONTRACT_REQ_CODE = 2;
+}
--- ContactsManager/app/src/main/java/com/politehnica/alexbudau/contactsmanager/MainActivity.java
+++ ContactsManager/app/src/main/java/com/politehnica/alexbudau/contactsmanager/MainActivity.java
@@ -1,15 +1,117 @@
 package com.politehnica.alexbudau.contactsmanager;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button showButton;
+    private Button saveButton;
+    private Button cancelButton;
+    private EditText nameEditText;
+    private EditText numberEditText;
+    private EditText emailEditText;
+    private EditText addressEditText;
+    private EditText jobEditText;
+    private EditText companyEditText;
+    private EditText websiteEditText;
+    private EditText imEditText;
+    private LinearLayout container;
+
+    private Intent intentFromParent;
+    private Intent intentToParent;
+
+    private boolean isContainerVisible;
+
+    private class ButtonClickListener implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.showButton :
+                    Button b = (Button) v;
+                    if (!isContainerVisible) {
+                        b.setText(R.string.hideFieldsButton);
+                        container.setVisibility(View.VISIBLE);
+                        isContainerVisible = true;
+                    }
+                    else {
+                        b.setText(R.string.showFieldsButton);
+                        container.setVisibility(View.GONE);
+                        isContainerVisible = false;
+                    }
+                    break;
+                case R.id.saveButton :
+                    Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
+                    intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
+
+                    String name = nameEditText.getText().toString();
+                    String phoneNumber = numberEditText.getText().toString();
+                    String email = emailEditText.getText().toString();
+                    String address = addressEditText.getText().toString();
+                    String jobTitle = jobEditText.getText().toString();
+                    String company = companyEditText.getText().toString();
+                    String website = websiteEditText.getText().toString();
+                    String im = imEditText.getText().toString();
+
+                    if (name != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.NAME, name);
+                    }
+                    if (phoneNumber != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.PHONE, phoneNumber);
+                    }
+                    if (email != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.EMAIL, email);
+                    }
+                    if (address != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.POSTAL, address);
+                    }
+                    if (jobTitle != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.JOB_TITLE, jobTitle);
+                    }
+                    if (company != null) {
+                        intent.putExtra(ContactsContract.Intents.Insert.COMPANY, company);
+                    }
+
+                    ArrayList<ContentValues> contactData = new ArrayList<ContentValues>();
+                    if (website != null) {
+                        ContentValues websiteRow = new ContentValues();
+                        websiteRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Website.CONTENT_ITEM_TYPE);
+                        websiteRow.put(ContactsContract.CommonDataKinds.Website.URL, website);
+                        contactData.add(websiteRow);
+                    }
+                    if (im != null) {
+                        ContentValues imRow = new ContentValues();
+                        imRow.put(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Im.CONTENT_ITEM_TYPE);
+                        imRow.put(ContactsContract.CommonDataKinds.Im.DATA, im);
+                        contactData.add(imRow);
+                    }
+                    intent.putParcelableArrayListExtra(ContactsContract.Intents.Insert.DATA, contactData);
+                    startActivityForResult(intent, Constants.CONTACTS_CONTRACT_REQ_CODE);
+                    break;
+                case R.id.cancelButton :
+                    if (intentFromParent != null) {
+                        setResult(RESULT_CANCELED, new Intent());
+                        finish();
+                    }
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +128,33 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        showButton = (Button) findViewById(R.id.showButton);
+        saveButton = (Button) findViewById(R.id.saveButton);
+        cancelButton = (Button) findViewById(R.id.cancelButton);
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        numberEditText = (EditText) findViewById(R.id.numberEditText);
+        emailEditText = (EditText) findViewById(R.id.emailEditText);
+        addressEditText = (EditText) findViewById(R.id.addressEditText);
+        jobEditText = (EditText) findViewById(R.id.jobEditText);
+        companyEditText = (EditText) findViewById(R.id.companyEditText);
+        websiteEditText = (EditText) findViewById(R.id.websiteEditText);
+        imEditText = (EditText) findViewById(R.id.imEditText);
+        container = (LinearLayout) findViewById(R.id.container);
+
+        View.OnClickListener clickListener = new ButtonClickListener();
+
+        showButton.setOnClickListener(clickListener);
+        saveButton.setOnClickListener(clickListener);
+        cancelButton.setOnClickListener(clickListener);
+
+        intentFromParent = getIntent();
+
+        if (intentFromParent != null) {
+            String phoneNumber = intentFromParent.getStringExtra(Constants.PHONE_NUMBER_EXTRA);
+            intentToParent = new Intent();
+            numberEditText.setText(phoneNumber);
+        }
     }
 
     @Override
@@ -48,5 +177,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (Constants.CONTACTS_CONTRACT_REQ_CODE == requestCode) {
+            setResult(resultCode, intentToParent);
+            finish();
+        }
     }
 }
